7d50e29d8afb94f5c3edc11ce6c41173d87181c2
Yidan Xu <42988802+yidanxu@users.noreply.github.com>
2021-05-19 03:46:11 -0400

    Updated map view
    Added legend, time slider, and show time on the top of the graph.

--- choro_map.js
+++ choro_map.js
@@ -49,11 +49,8 @@ Promise.all([d3.json("data/countries-110m-noant.json"),
             d.pop = getPopulation(wmPop, d.id, '2019');
              return( fillScale(d.pop));
       })
-          .on("mouseenter", (event, d) => {
-            console.log(event)
-            console.log("entered")
-          });
-
+          .on("mouseout",  mouseLeavesPlot)
+          .on("mouseenter", mouseEntersPlot)
 
 
       wmMap.append("path").datum(wmMesh)
@@ -61,7 +58,112 @@ Promise.all([d3.json("data/countries-110m-noant.json"),
           .attr("d", wmPath)
 
 
+        function mouseEntersPlot(event,d) {
 
+            d3.selectAll(".country")
+                .transition()
+                .duration(200)
+                .style("opacity", 0.3)
+
+            d3.select(this)
+                .transition()
+                .duration(200)
+                .style("opacity", 1)
+        }
+
+        function mouseLeavesPlot() {
+
+            d3.selectAll(".country")
+                .transition()
+                .duration(200)
+                .style("opacity", 1)
+
+        };
+
+      //Legend
+        var w = 300, h = 130;
+        let highColor = d3.schemeBlues[9][8]
+        let lowColor = d3.schemeBlues[9][0]
+
+        var key = d3.select("#world-chart")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h)
+            .attr("class", "legend");
+
+        var legend = key.append("defs")
+            .append("svg:linearGradient")
+            .attr("id", "gradient")
+            .attr("x1", "0%")
+            .attr("y1", "100%")
+            .attr("x2", "100%")
+            .attr("y2", "100%")
+            .attr("spreadMethod", "pad");
+
+        legend.append("stop")
+            .attr("offset", "0%")
+            .attr("stop-color", lowColor)
+            .attr("stop-opacity", 1);
+
+        legend.append("stop")
+            .attr("offset", "100%")
+            .attr("stop-color", highColor)
+            .attr("stop-opacity", 1);
+
+        key.append("rect")
+            .attr("width", w)
+            .attr("height", h-100)
+            .style("fill", "url(#gradient)")
+            .attr("transform", "translate(0,10)");
+
+        var x = d3.scaleLinear()
+            .range([0, w])
+            .domain(d3.extent(wmPop.map(d => d.Total)))
+
+
+        var xAxis = d3.axisBottom(x);
+
+        key.append("g")
+            .attr("class", "x_axis")
+            .attr("transform", "translate(0,40)")
+            .call(xAxis.tickFormat(d3.format(".0s")))
+
+
+        //Time Slider
+
+        let dataTime = d3.range(0, 20).map(function(d) {
+            return new Date(2001 + d, 10, 3);
+        });
+
+        let sliderTime = d3
+            .sliderBottom()
+            .min(d3.min(dataTime))
+            .max(d3.max(dataTime))
+            .step(1000 * 60 * 60 * 24 * 365)
+            .width(500)
+            .tickFormat(d3.timeFormat('%Y'))
+            .tickValues(dataTime)
+            .default(new Date(2008, 10, 3))
+            .on('onchange', val => {
+                d3.select('p#value-time').text(d3.timeFormat('%Y')(val));
+                  let new_year = d3.select('p#value-time').text();
+             //   d3.select('p#value-time').text(d3.timeFormat('%Y')(val));
+              //  let new_year = d3.select('p#value-time').text();
+             //   rerender(new_year);
+            });
+
+        let gTime = d3
+            .select("div#world-chart")
+            .append('svg')
+            .attr("class","slider")
+            .attr('width', 600)
+            .attr('height', 100)
+            .append('g')
+            .attr('transform', 'translate(30,10)');
+
+        gTime.call(sliderTime);
+        d3.select('p#value-time').text(d3.timeFormat('%Y')(sliderTime.value()));
+        d3.select('p#value-time').text(d3.timeFormat('%Y')(sliderTime.value()));
 
 
     }); //end of then
@@ -69,9 +171,8 @@ Promise.all([d3.json("data/countries-110m-noant.json"),
 function getPopulation(data, id, year) {
   let val = data.filter( d => d['country.code'] === id & d.Year == year);
 
-  if(val.length == 0) {    console.log(id); return null;}
+  if(val.length == 0) {console.log(id); return null;}
   else {
-
     return val[0].Total;
   }
 }
