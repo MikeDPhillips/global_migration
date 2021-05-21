c8bb841526cab148b5632f7cc09f8aad376683bc
Yidan Xu <42988802+yidanxu@users.noreply.github.com>
2021-05-21 18:13:48 -0400

    Revert "j"
    This reverts commit 52b1627c77c4e381d2a13bc1841662ed5c90fa06.

--- fancy_chart.js
+++ fancy_chart.js
@@ -1,18 +1,26 @@
 const fancyWidth = 600
-const fancyHeight = fancyWidth*.618
+const fancyHeight = 600
 const fancyMargin = { top:10, right:10, bottom:10, left:10}
-const fancy_chart_Width = wmWidth - wmMargin.left -wmMargin.right
-const fancy_chart_Height = wmHeight - wmMargin.top - wmMargin.bottom
+const fancy_chart_Width = fancyWidth - fancyMargin.left -fancyMargin.right
+const fancy_chart_Height = fancyHeight - fancyMargin.top - fancyMargin.bottom
 
-let fancySvg = d3.select("#fancy-chart").append("svg")
+
+let fsvgContainer = d3.select('#fancy-chart');
+let fancySvg = fsvgContainer.append("svg")
     .attr("id", "fancy")
     .attr("width", fancyWidth)
     .attr("height", fancyHeight)
 
 let fancyChart = fancySvg.append("g").attr("transform", `translate(${fancyMargin.left}, ${fancyMargin.top})`)
 
-Promise.all([
-    d3.csv("data/pop_data_long.csv", d3.autoType())]
+let legends = d3.select("#legendDiv")
+    .append("svg")
+    .attr('width', 600)
+    .attr('height',100)
+    .append('g')
+    .attr('transform', 'translate(0, 30)')
+
+Promise.all([d3.csv("data/pop_data_long.csv", d3.autoType())]
 ).then( ([allPop]) => {
 
    allPop.forEach( d => {
@@ -22,13 +30,13 @@ Promise.all([
 
     console.log(allPop)
 
-  let xKey = "growth_rate"
-  let yKey = "natural_increase"
-  let year = 2016
+    let xKey = "natural_increase"
+    let yKey = "growth_rate"
+    let year = 2016
 
-  let filteredX= allPop.filter(function(d){return d.indicator == "growth_rate" })
+  let filteredX= allPop.filter(function(d){return d.indicator == xKey })
       .filter(d => d.year==year)
-  let filteredY= allPop.filter(function(d){return d.indicator == "natural_increase" })
+  let filteredY= allPop.filter(function(d){return d.indicator == yKey })
       .filter(d => d.year==year)
 
   console.log(filteredX)
@@ -41,7 +49,8 @@ Promise.all([
       country : filteredX[i].country,
       year : filteredX[i].year,
       xValue : filteredX[i].value,
-      yValue : filteredY[i].value
+      yValue : filteredY[i].value,
+        region : filteredX[i].region
     });
   }
 
@@ -59,22 +68,103 @@ Promise.all([
 
     console.log(yExtent)
 
-    let yAxis = d3.axisLeft(yScale).ticks(10).tickFormat(d3.format(".3f"))
+    let yAxis = d3.axisLeft(yScale).ticks(10).tickFormat(d3.format(".1f"))
     let xAxis = d3.axisBottom(xScale).tickFormat(d3.format(".1f"))
 
 
-    fancySvg.append("g")
-        .attr("transform","translate("+fancyMargin.left+","+(fancyMargin.top)+")")
-        .attr("transform","translate(50,"+(fancyMargin.top-10)+")") //30
+    fancyChart.append("g")
+        //.attr("transform","translate("+fancyMargin.left+","+(fancyMargin.top)+")")
+        //.attr("transform","translate(50,"+(fancyMargin.top-10)+")") //30
+        .attr("transform","translate(90,10)")
         .call(yAxis);
 
-    fancySvg.append("g")
+    fancyChart.append("g")
         .attr("class","xAxis")
-       // .attr("transform","translate("+fancyMargin.left+","+(fancy_chart_Height+fancyMargin.top)+")")
-        .attr("transform","translate(50,"+(fancy_chart_Height-220)+")") //20
+        .attr("transform","translate("+fancyMargin.left+","+(fancy_chart_Height+fancyMargin.top)+")")
+        .attr("transform","translate(10,341)")
         .call(xAxis);
 
-    fancySvg.append('g')
+    fancyChart.append("text")
+        .attr("class","fancyLabel")
+        .attr("text-anchor", "end")
+        .attr("x", 580)
+        .attr("y", 380)
+        .text("Natural Increase Rate");
+
+    //add y-axis label
+    fancyChart.append("text")
+        .attr("class","fancyLabel")
+        .attr("text-anchor", "end")
+        //.attr("transform", "rotate(-90)")
+        .attr("x", 85)
+        .attr("y", 30)
+        .text("Growth Rate");
+
+    //build legends
+    let title = legends.append('text')
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('fill', 'black')
+        .attr('font-family', 'Helvetica Neue, Arial')
+        .attr('font-weight', 'bold')
+        .attr('font-size', '12px')
+        .text('Region');
+
+    let legendName = [
+        { index: 0, label: 'Africa' },
+        { index: 1, label: 'Asia' },
+        { index: 2, label: 'Europe' },
+        { index: 3, label: 'Latin America' },
+        { index: 4, label: 'North America' },
+        { index: 5, label: 'Oceania' },
+    ];
+
+    console.log(legendName)
+
+    let fancy_colorScale = d3.scaleOrdinal().domain(legendName)
+        .range(['#edc949','#4e79a7','#f28e2c','#e15759','#76b7b2','#59a14f'])
+
+    let entries = legends
+        .selectAll('g')
+        .data(legendName)
+        .join('g')
+        .attr('transform', d => `translate(${20 + d.index * 100},20)`);
+
+    let symbols = entries.append('circle')
+        .attr('cx',5) // <-- offset symbol x-position by radius
+        .attr('r', 5)
+        .style('fill', d=>fancy_colorScale(d.label));
+
+    let labels = entries
+        .append('text')
+        .attr('x',14)
+        .attr('y',4)
+        .attr('fill', 'black')
+        .attr('font-family', 'Helvetica Neue, Arial')
+        .attr('font-size', '11px')
+        .text(d=>d.label);
+
+
+    let tooltip = fsvgContainer
+        .append("div")
+        .attr("class","tooltip")
+        .style("position", "absolute")
+        //.style("font-family", "'Open Sans', sans-serif")
+        //.style("font-size", "12px")
+        .style("z-index", "10")
+        .style("background-color", "#A7CDFA")
+        .style("color", "#B380BA")
+        .style("border", "solid")
+        .style("border-color", "#A89ED6")
+        .style("padding", "5px")
+        .style("border-radius", "2px")
+        .style("visibility", "hidden");
+
+    tooltip;
+
+   // let fancy_colorScale = d3.scaleOrdinal().range(d3.schemeTableau10)
+
+    fancyChart.append('g')
         .selectAll("dot")
         .data(plotData)
         .enter()
@@ -84,7 +174,6 @@ Promise.all([
           return(xScale(d.xValue))
         })
         .attr("cy", d => yScale(d.yValue))
-<<<<<<< HEAD
         .attr("r", 7)
         .style("fill", d=>fancy_colorScale(d.region))
         .style("opacity", "0.5")
@@ -120,9 +209,3 @@ Promise.all([
 
 
 
-=======
-        .attr("r", 5)
-        .style("fill", "#69b3a2")
-
-})
->>>>>>> parent of bd55aee (add fancy chart)
