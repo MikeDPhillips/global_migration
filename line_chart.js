6f0d3928b48fe90c14166d4d1faf87e1bc650d73
Yidan Xu <42988802+yidanxu@users.noreply.github.com>
2021-05-21 21:39:27 -0400

    new push for line chart
    line chart update

--- line_chart.js
+++ line_chart.js
@@ -4,9 +4,15 @@ const lineMargin = { top:10, right:10, bottom:10, left:10}
 const line_chart_Width = lineWidth - lineMargin.left -lineMargin.right
 const line_chart_Height = lineHeight - lineMargin.top - lineMargin.bottom
 
+let lineSvgContainer = d3.select('#country-chart');
 let lineSvg = lineSvgContainer.append("svg")
     .attr("id", "lineSvg")
     .attr("width", lineWidth)
     .attr("height", lineHeight)
 
 let lineChart = lineSvg.append("g").attr("transform", `translate(${lineMargin.left}, ${lineMargin.top})`)
+
+d3.csv("pop_data_long.csv",d3.autoType).then((data)=>{
+    console.log(data)
+})
+
